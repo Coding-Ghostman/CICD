afb604a0662467fe4a483665d6bba792000bf5bb
Your Name <you@example.com>
2023-12-15 14:54:15 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -45,5 +45,5 @@ def index():
 
 if __name__ == '__main__':
 
-    # app.run(host="0.0.0.0", port=8080) #for deployment run
-    app.run(host="127.0.0.1", port=8080,debug=True) # for local run
+    app.run(host="0.0.0.0", port=8080) #for deployment run
+    # app.run(host="127.0.0.1", port=8080,debug=True) # for local run
